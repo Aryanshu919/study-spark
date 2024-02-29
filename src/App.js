9d53c03c5c6ebfeaedca49f3b79274d03abed005
Aryan Kumar <aryanshu919@gmail.com>
2024-02-29 20:31:55 +0530

    add some files in src

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
       
     </div>
   );
