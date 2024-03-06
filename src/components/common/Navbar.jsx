807ac6054350267060b498c6a036d006a1b2dd50
Aryan Kumar <aryanshu919@gmail.com>
2024-03-06 22:02:51 +0530

    add navbar is project

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
     className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblue-700 transition-all duration-200`}
     >
         <div className="flex w-11/12 max-w-maxContent items-center justify-between">
-         {/* Logo */}
-        {/* <Link to="/">
+         Logo
+         <Link to="/">
           <img src={logo} alt="Logo" width={160} height={32} loading="lazy" />
-        </Link> */}
+        </Link>
       
         </div>
 
