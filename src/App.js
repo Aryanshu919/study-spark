b59ce458e74ee42e7eebe9d2fbacd294428bd789
Aryan Kumar <aryanshu919@gmail.com>
2024-03-17 20:29:10 +0530

    added some files

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Navbar from './components/common/Navbar'
 import Signup from './pages/Signup';
 import ForgotPassword from './pages/ForgotPassword';
 import UpdatePassword from './pages/UpdatePassword';
+import VerifyEmail from './pages/VerifyEmail';
+import About from './pages/About';
 function App() {
   return (
     <div className='w-screen min-h-screen bg-richblue-500 flex flex-col font-inter'>
@@ -18,6 +20,8 @@ function App() {
         <Route path='/signup' element={<Signup />}></Route>
         <Route path='/forgot-password' element={<ForgotPassword />}></Route>
         <Route path='/update-password/:id' element={<OpenRoute><UpdatePassword /></OpenRoute>}></Route>
+        <Route path='/verify-email' element={<VerifyEmail />}></Route>
+        <Route path='/about' element={<OpenRoute><About /></OpenRoute>}></Route>
         
       </Routes>
       
